578becf81314e5aafafa214570ed41b588bbfb4c
Esduardo <eduartrob@gmail.com>
2023-04-17 09:16:39 -0600

    Delpy de Api

--- src/main/java/com/basedatos/basededatos/models/UserModel.java
+++ src/main/java/com/basedatos/basededatos/models/UserModel.java
@@ -8,11 +8,13 @@ public class UserModel {
     @Column(name = "id",updatable = false, nullable = false,  unique = true)
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
-    @Column(name = "last_name")
-    private String lastName;
+    @Column(name = "name")
+    private String name;
 
-    @Column(name = "first_name")
-    private String firstName;
+    @Column(name = "email")
+    private String email;
+    @Column(name = "password")
+    private String password;
 
 
     public long getId() {
@@ -23,20 +25,27 @@ public class UserModel {
         this.id = id;
     }
 
-    public String getLastName() {
-        return lastName;
+    public String getName() {
+        return name;
     }
 
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
+    public void setName(String name) {
+        this.name = name;
     }
 
-    public String getFirstName() {
-        return firstName;
+    public String getEmail() {
+        return email;
     }
 
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
+    public void setEmail(String email) {
+        this.email = email;
     }
-    
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
